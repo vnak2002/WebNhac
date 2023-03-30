cb7088eac00287aedd4014fc8bade87a24d4681e
Axeon <khoabmt12354@gmail.com>
2023-03-30 08:23:34 +0700

    Add project

---
+++ WebSach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSach.Startup))]
+namespace WebSach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
